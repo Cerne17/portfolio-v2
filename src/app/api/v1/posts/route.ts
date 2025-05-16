3f37fb6b0d92c52f8df7d663598e623f66e81298
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2025-05-16 20:18:26 -0300

    fix: disables eslint error

--- src/app/api/v1/posts/route.ts
+++ src/app/api/v1/posts/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(request: Request) {
   // Takes request for filtering and/or pagination
   // To be added
